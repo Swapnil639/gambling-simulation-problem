24616588f63be78a29c1cf83afcd13c33eff5b2c
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-06 13:30:30 +0530

    Added :Check if player as won or loose

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,8 +1,26 @@
 package com.bridgelabz;
+
 public class GamblingSimulator {
+    public static final int STAKE_PER_DAY = 100;
+    public static final int BET = 1;
+    public static final int WIN = 1;
+    public static final int LOOSE = 0;
+
+    public static void dailyBet() {
+        int cash = STAKE_PER_DAY;
+        int option = (int) ((Math.random() * 10) % 2);
+        if (option == WIN) {
+            cash += BET;
+            System.out.println("Player has won " + cash);
+        } else if (option == LOOSE) {
+            cash -= BET;
+            System.out.println("Player has loose " + cash);
+        }
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling simulation");
-        final int STAKE_PER_DAY = 100;
-        final int BET = 1;
+        dailyBet();
     }
+
 }
