4c1b3d19e9520bdb2c6ab2c68e96e59cbbc4f5e1
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-06 13:17:25 +0530

    Added :Initialize Stake and Bet

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,7 +1,8 @@
 package com.bridgelabz;
-
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling simulation");
+        final int STAKE_PER_DAY = 100;
+        final int BET = 1;
     }
 }
