a01eded3b19ece66fe6eae855512216dba0105af
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-14 23:46:07 +0530

    Added :Days won or lose in month

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -3,18 +3,18 @@ package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
-        int stake = 100;
         int bet = 1;
         int wins = 0;
         int lose = 0;
-        for (int day = 1; day <= 20; day++) {
+        for (int day = 1; day <= 30; day++) {
+            int stake = 100;
             while (stake > 50 && stake < 150) {
                 double gameCheck = Math.floor(Math.random() * 10) % 2;
                 if (bet == gameCheck) {
-                    // System.out.println("you won the game");
+                    //System.out.println("you won the game");
                     stake++;
                 } else {
-                    // System.out.println("you lost the game")
+                    //System.out.println("you lost the game");
                     stake--;
                 }
             }
@@ -28,12 +28,15 @@ public class GamblingSimulator {
             }
         }
         int finalAmount = wins * 50 - lose * 50;
-        if (finalAmount > 0)
-            System.out.println("After 20 days Gambler won:" + finalAmount);
-        else if (finalAmount < 0)
-            System.out.println("After 20 days Gambler lost:" + finalAmount);
-        else
+        if (finalAmount > 2000) {
+            int profitAmount = finalAmount - 2000;
+            System.out.println("After 30 days Gambler won:" + profitAmount);
+        } else if (finalAmount < 0) {
+            int lossAmount = 2000 - finalAmount;
+            System.out.println("After 30 days Gambler lost:" + lossAmount);
+        }else
             System.out.println("No profit or lose");
+        System.out.println("In this month " + wins + " Days Won. " + lose + " Days lost");
 
     }
 }
