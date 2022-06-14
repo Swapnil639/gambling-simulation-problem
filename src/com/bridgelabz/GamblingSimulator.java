cf3f89153e2e162a98f5551de42900c2482186f9
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-06-14 23:11:02 +0530

    Added : Gambler make bet either win or loose

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,26 +1,20 @@
 package com.bridgelabz;
 
 public class GamblingSimulator {
-    public static final int STAKE_PER_DAY = 100;
-    public static final int BET = 1;
-    public static final int WIN = 1;
-    public static final int LOOSE = 0;
-
-    public static void dailyBet() {
-        int cash = STAKE_PER_DAY;
-        int option = (int) ((Math.random() * 10) % 2);
-        if (option == WIN) {
-            cash += BET;
-            System.out.println("Player has won " + cash);
-        } else if (option == LOOSE) {
-            cash -= BET;
-            System.out.println("Player has loose " + cash);
-        }
-    }
-
     public static void main(String[] args) {
-        System.out.println("Welcome to Gambling simulation");
-        dailyBet();
+        System.out.println("Welcome to Gambling Simulator");
+        int bet = 1;
+        int totalAmount = 100;
+        double gameCheck = Math.floor(Math.random() * 10) % 2;
+        if (bet == gameCheck) {
+            System.out.println("Gambler win");
+            totalAmount++;
+        } else {
+            System.out.println("Gambler loose");
+            totalAmount--;
+
+        }
+        System.out.println("Total amount :" + totalAmount);
     }
 
 }
