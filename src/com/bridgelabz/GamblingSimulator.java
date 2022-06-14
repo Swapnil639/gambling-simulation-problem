32ee9623264dbb25f681db3a23d7e59d2ff04436
Swapnil Patil <102506228+Swapnil639@users.noreply.github.com>
2022-06-14 23:08:49 +0530

    Added : Start game

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -1,8 +1,9 @@
 package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
-        System.out.println("Welcome to Gambling simulation");
-        final int STAKE_PER_DAY = 100;
-        final int BET = 1;
+        System.out.println("Welcome to Gambling Simulator");
+        int betPerGame = 1;
+        int stakeperday=100;
+
     }
 }
