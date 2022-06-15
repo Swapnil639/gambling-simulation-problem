cc6c39ee2dc90178e3f4dfb8da41ac22ef2534e8
Swapnil639 <swapnilpatil631997@gmail.com>
2022-06-15 07:37:12 +0530

    Added :Days won or lose in month

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -3,19 +3,40 @@ package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println("Welcome to Gambling Simulator");
-        int totalAmount = 100;
         int bet = 1;
-        while (totalAmount > 50 && totalAmount < 150) {
-            double gameCheck = Math.floor(Math.random() * 10) % 2;
-            if (bet == gameCheck) {
-                System.out.println("you won the game");
-                totalAmount++;
+        int wins = 0;
+        int lose = 0;
+        for (int day = 1; day <= 30; day++) {
+            int stake = 100;
+            while (stake > 50 && stake < 150) {
+                double gameCheck = Math.floor(Math.random() * 10) % 2;
+                if (bet == gameCheck) {
+                    //System.out.println("you won the game");
+                    stake++;
+                } else {
+                    //System.out.println("you lost the game");
+                    stake--;
+                }
+            }
+            if (stake == 50) {
+                lose++;
+                System.out.println("Day" + day + " Lost");
             } else {
-                System.out.println("you lost the game");
-                totalAmount--;
+                wins++;
+                System.out.println("Day" + day + " Won");
+
             }
         }
-        System.out.println("Total amount :" + totalAmount);
+        int finalAmount = wins * 50 - lose * 50;
+        if (finalAmount > 2000) {
+            int profitAmount = finalAmount - 2000;
+            System.out.println("After 30 days Gambler won:" + profitAmount);
+        } else if (finalAmount < 0) {
+            int lossAmount = 2000 - finalAmount;
+            System.out.println("After 30 days Gambler lost:" + lossAmount);
+        }else
+            System.out.println("No profit or lose");
+        System.out.println("In this month " + wins + " Days Won. " + lose + " Days lost");
+
     }
 }
-
